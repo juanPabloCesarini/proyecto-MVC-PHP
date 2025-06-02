4e31cedb7300a5d09a5f4bb6d38da9a1cd666903
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-06-02 12:14:46 -0300

    estructura del proyecto y configuraciones iniciales

---
+++ config/app.php
@@ -0,0 +1,6 @@
+<?php
+    const APP_URL="http://localhost/proyecto-MVC-PHP/";
+    const APP_NAME="CRUD PHP MYSQL";
+    const APP_SESSION_NAME="CRUD"; //NOMBRES CORTOS Y SIN ESPACIOS
+
+    date_default_timezone_set("America/Argentina/Buenos_Aires");
