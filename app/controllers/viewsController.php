c90f800e9d1db5d9d371a8dc8c0522531aba165d
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-06-04 11:34:55 -0300

    controlador  de vistas

--- app/controllers/viewsController.php
+++ app/controllers/viewsController.php
@@ -5,5 +5,14 @@
 
     class viewsController extends viewsModel{
 
+        public function obtenerVistasControlador($vista){
+
+            if ($vista!="") {
+                $respuesta =$this->obtenerVistasModelo($vista);
+            } else {
+                $respuesta ="login";
+            }
+            return $respuesta;
+        }
     }
     
