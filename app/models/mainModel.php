51c2602ab7a8fbd56759f09af72f6b207c9ac29c
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-06-05 11:03:04 -0300

    comenzando con el código del mainModel

---
+++ app/models/mainModel.php
@@ -0,0 +1,55 @@
+<?php
+    namespace app\models;
+    use \PDO;
+
+    if (file_exists(__DIR__."/../../config/server.php")){
+        require_once __DIR__."/../../config/server.php";
+    }
+
+    class mainModel{
+        private $server = DB_SERVER;
+        private $db = DB_NAME;
+        private $user = DB_USER;
+        private $pass = DB_PASS;
+
+        protected function conectar(){
+            $conexion = new PDO("mysql:host=".$this->server.";dbname=".$this->db,$this->user,$this->pass);
+            $conexion->exec("SET CHARACTER utf8");
+            return $conexion;
+        }
+
+        protected function ejecutarConsulta($consulta){
+            $sql = $this->conectar()->prepare($consulta);
+            $sql->execute();
+            return $sql;
+        }
+
+        public function limpiarCadena($cadena){
+
+            $palabras=["<script>","</script>","<script src","<script type=","SELECT * FROM","SELECT "," SELECT ","DELETE FROM","INSERT INTO","DROP TABLE","DROP DATABASE","TRUNCATE TABLE","SHOW TABLES","SHOW DATABASES","<?php","?>","--","^","<",">","==","=",";","::"];
+
+            $cadena = trim($cadena);
+
+            $cadena = striolashes($cadena);
+
+            foreach ($$palabras as $palabra) {
+                $cadena = str_ireplace($palabra, "",$cadena);
+                $cadena = trim($cadena);
+                $cadena = striolashes($cadena);
+            }
+            return $cadenas;
+        }
+
+        protected function verificarDatos($filtro, $cadena){
+            if (preg_match("/^".$filtro."$/",$cadena)) {
+                return false;
+            } else {
+                return true;
+            }
+            
+        }
+
+        protected function guardarDatos($tabla,$datos){
+            
+        }
+    }
