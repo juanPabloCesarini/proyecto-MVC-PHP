75096b5c415d1100095e97b0620841a96c0176cd
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-06-05 17:44:20 -0300

    mainModel con métodos para codificar el CRUD

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -50,6 +50,85 @@
         }
 
         protected function guardarDatos($tabla,$datos){
+            $query = "INSERT INTO $tabla (";
+            $c = 0;
+            foreach($datos as $clave){
+                if ($c>=1) {
+                   $query.=",";
+                }
+                $query.=$clave["campo_nombre"];
+                $c++;
+            }
+            $query.=") VALUES (";
+
+             $c = 0;
+            foreach($datos as $clave){
+                if ($c>=1) {
+                   $query.=",";
+                }
+                $query.=$clave["campo_marcador"];
+                $c++;
+            }
+            $query.=")";
+            $sql = $this->conectar()->prepare($query);
+
+            foreach ($datos as $clave) {
+                $sql->bindParam($clave['campo_marcador'],$clave['campo_valor']);
+            }
+            $sql->execute();
+
+            return $sql;
+        }
+
+        public function seleccionarDatos($tipo,$tabla,$campo,$id){
+            $tipo = $this->limpiarCadena($tipo);
+            $tabla = $this->limpiarCadena($tabla);
+            $campo = $this->limpiarCadena($campo);
+            $id = $this->limpiarCadena($id);
+
+            if ($tipo=="Unico") {
+                $sql = $this->conectar()->prepare("SELECT * FROM $tabla WHERE $campo =:ID");
+                $sql->bindParam(":ID",$id);
+            } elseif($tipo=="Normal") {
+                $sql = $this->conectar()->prepare("SELECT $campo FROM $tabla ");
+            }
+
+             $sql->execute();
+
+            return $sql;        
+        }
+
+          protected function actualizarDatos($tabla,$datos,$condicion){
+            $query = "UPDATE $tabla SET ";
+            $c = 0;
+            foreach($datos as $clave){
+                if ($c>=1) {
+                   $query.=",";
+                }
+                $query.=$clave["campo_nombre"]."=".$clave["campo_marcador"];
+                $c++;
+            }
+            $query.=" WHWRE ".$condicion["condicion_campo"]."=".$condicion["condición_marcador"];
             
+            $sql = $this->conectar()->prepare($query);
+
+            foreach ($datos as $clave) {
+                $sql->bindParam($clave['campo_marcador'],$clave['campo_valor']);
+            }
+            
+            $sql->bindParam($condicion['condicion_marcador'],$condicion['condicion_valor']);
+
+            $sql->execute();
+
+            return $sql;
+        }
+
+        protected function eliminarRegistro($tabla,$campo,$id){
+            $sql = $this->conectar()->prepare("DELETE FROM $tabla WHERE $campo=:id");
+            $sql->bindParam(":id",$id);
+            $sql->execute();
+
+            return $sql;
+
         }
     }
