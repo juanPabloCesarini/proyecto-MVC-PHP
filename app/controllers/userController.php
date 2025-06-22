9fe2cab0bad3e3aab9811cbb1c16e5e6f26e4464
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-06-22 12:19:08 -0300

    método registro de usuario, validación de imagenes

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -130,7 +130,88 @@ class userController extends mainModel {
 
         #Directorio de imágenes#
 
-        $img_dir ="../views/fotos/";
+        $img_dir = '../views/fotos/';
+
+        # Comprobar si hay imagenes para subir al servidor #
+
+        if ( $_FILES[ 'usuario_foto' ][ 'name' ] = !'' && $_FILES[ 'usuario_foto' ][ 'size' ]>0 ) {
+            #Crar directorio #
+            if ( !file_exists( $img_dir ) ) {
+                if ( !mkdir( $img_dir, 0777 ) ) {
+                    $alerta = [
+                        'tipo' => 'simple',
+                        'titulo' => 'Error!',
+                        'texto'=>'Error al crear el directorio!',
+                        'icono'=>'error'
+                    ];
+                    return json_encode( $alerta );
+                    exit();
+
+                }
+            }
+            # Verificando formato de imagenes #
+
+            if ( mime_content_type( $_FILES[ 'usuario_foto' ][ 'tmp_name' ] ) != 'image/jpeg' &&
+            mime_content_type( $_FILES[ 'usuario_foto' ][ 'tmp_name' ] ) != 'image/jpg' &&
+            mime_content_type( $_FILES[ 'usuario_foto' ][ 'tmp_name' ] ) != 'image/png' ) {
+                $alerta = [
+                    'tipo' => 'simple',
+                    'titulo' => 'Error!',
+                    'texto'=>'La imagen no tiene un formato permitido!',
+                    'icono'=>'error'
+                ];
+                return json_encode( $alerta );
+                exit();
+            }
+
+            # Verificando peso de la imágen #
+
+            if ( ( $_FILES[ 'usuario_foto' ][ 'size' ] /1024 )>5120 ) {
+                $alerta = [
+                    'tipo' => 'simple',
+                    'titulo' => 'Error!',
+                    'texto'=>'La imagen tiene un peso superior al permitido',
+                    'icono'=>'error'
+                ];
+                return json_encode( $alerta );
+                exit();
+            }
+
+            #Nombre de la imagen#
+
+            $foto=str_ireplace(" ","_",$nombre);
+            $foto=$foto."_".rand(0,100);
+
+            #Extensión de la imagen#
+
+            switch ($mime_content_type( $_FILES[ 'usuario_foto' ][ 'tmp_name' ] )) {
+                case 'image/jpeg':
+                    $foto=$foto.".jpeg";
+                    break;
+                case 'image/jpg':
+                    $foto=$foto.".jpg";
+                    break;
+                case 'image/png':
+                    $foto=$foto.".png";
+                    break;
+            }
+            chmod($img_dir,0777);
+
+            #Moviendo imagen al directorio #
+            if(!move_uploaded_file($_FILES[ 'usuario_foto' ][ 'tmp_name' ],$img_dir.$foto)){
+                $alerta = [
+                    'tipo' => 'simple',
+                    'titulo' => 'Error!',
+                    'texto'=>'La imagen no se pudo guardar',
+                    'icono'=>'error'
+                ];
+                return json_encode( $alerta );
+                exit();
+            }
+
+        } else {
+            $foto = '';
+        }
 
     }
 }
